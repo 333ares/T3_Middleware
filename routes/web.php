659edc1d2b27355c0f5131d115a4807e9a083969
Ares <aresgp5335@gmail.com>
2026-01-08 22:40:36 +0100

    Funcionamiento explicado

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ Route::get('/', function () {
 Route::get('/adultos', function () {
     return '¡Felicidades! Eres mayor de 18 años.';
 })->middleware(VerifyAge::class);
+
+/* 
+Para hacer pruebas de las rutas ponemos en la url: 
+1. Edad inválida: http://127.0.0.1:8000/adultos?edad=0 
+2. Menor de edad: http://127.0.0.1:8000/adultos?edad=10
+3. Mayor de edad: http://127.0.0.1:8000/adultos?edad=18
+4. Ruta publica: http://127.0.0.1:8000/
+*/
