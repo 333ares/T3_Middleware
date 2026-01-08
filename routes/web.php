3da66c5d5d857e969cf71c7bb03a611fb3ba42e5
Ares <aresgp5335@gmail.com>
2026-01-08 22:37:06 +0100

    Comentarios rutas

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\VerifyAge;
 
+// Ruta pública sin restricciones
 Route::get('/', function () {
     return '¡Bienvenido/a a la web de Ares!';
 });
 
+// Ruta para mayores de 18
 Route::get('/adultos', function () {
     return '¡Felicidades! Eres mayor de 18 años.';
 })->middleware(VerifyAge::class);
