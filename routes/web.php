2df1f559056911fd9ba74c07a63ad6e3edde2bc5
Ares <aresgp5335@gmail.com>
2026-01-08 22:36:19 +0100

    Segunda ruta
    Verificacion de mayor de edad

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\VerifyAge;
 
 Route::get('/', function () {
     return '¡Bienvenido/a a la web de Ares!';
 });
+
+Route::get('/adultos', function () {
+    return '¡Felicidades! Eres mayor de 18 años.';
+})->middleware(VerifyAge::class);
